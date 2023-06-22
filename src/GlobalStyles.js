6fd868f5befd4b6460b686daf398e7029205f2b6
Nam Anh Mai <maithenamanh@gmail.com>
2023-06-22 15:31:05 -0700

    Add CSS reset as global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,35 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    /* Apply border-box to all elements and pseudo-elements ::before, ::after */
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    /* Remove default margin */
+    * {
+        margin: 0;
+    }
+
+    /* Use percentage-based heights*/
+    html, body {
+        height: 100%;
+    }
+
+    body {
+        /* Set default line height */
+        line-height: 1.5;
+        /* Font smoothing on MacOS browser */
+        -webkit-font-smoothing: antialiased;
+    }
+
+    /* Inherit fonts for form control*/
+    input, button, textarea, select {
+        font: inherit;
+    }
+
+`
+
+export default GlobalStyles;
