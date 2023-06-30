dc119e779587b2ed02b982cd183b16c8aadcd315
Nam Anh Mai <maithenamanh@gmail.com>
2023-06-30 12:01:56 -0700

    Add more book data and fields to book display

--- src/Book.js
+++ src/Book.js
@@ -3,21 +3,44 @@ import styled from 'styled-components/macro'
 
 const Book = ({name, author, imageSrc}) => {
   return (
-    <>
+    <Link>
       <ImageWrapper>
         <Image alt="" src={imageSrc} />
       </ImageWrapper>
-    </>
+      <Name>{name}</Name>
+      <Author>{author}</Author>
+    </Link>
   );
 };
 
+const Link = styled.a``;
+
 const ImageWrapper = styled.div`
   position: relative;
+  border-radius: 2%;
+  /* Hide image that extends beyong this wrapper */
+  overflow: hidden;
+
+  &:hover {
+    box-shadow: 4px 4px 10px hsl(180deg, 5%, 50%);
+  }
+
+  &:focus {
+    box-shadow: 4px 4px 10px hsl(180deg, 5%, 50%);
+  }
+  
 `;
 
 const Image = styled.img`
+  /* Set to block to cover all parent container space*/  
   display: block;
-  width: 30%;
+  /* Set width to be the same as parent content's */
+  width: 100%;
+  line-height: 0;
 `;
+
+const Name = styled.h2``;
+
+const Author = styled.h3``;
 
 export default Book;
--- src/data.js
+++ src/data.js
@@ -9,14 +9,64 @@ const BOOKS = [
     imageSrc: '/assets/aislyn.jpg'
   },
   {
-    name: 'All Plants Can\`t Live',
+    name: 'All Plants Can\'t Live',
     author: 'Ryan Jacson',
     imageSrc: '/assets/all_plants_cant_live.jpg'
+  },
+  {
+    name: 'The Great Azulene Casino',
+    author: 'Freddie Pearce',
+    imageSrc: '/assets/azulene_casino.jpg'
+  },
+  {
+    name: 'Passing Clouds Of Silver',
+    author: 'River Kirk',
+    imageSrc: '/assets/clouds_of_silver.jpg'
   },
   {
     name: 'Cluster',
     author: 'Jessica D. Palma',
     imageSrc: '/assets/cluster.jpg'
+  },
+  {
+    name: 'Crack The Code',
+    author: 'Patrick C. Harless',
+    imageSrc: '/assets/crack_the_code.jpg'
+  },
+  {
+    name: 'Fascination',
+    author: 'Kailee Singleton',
+    imageSrc: '/assets/fascination.jpg'
+  },
+  {
+    name: 'The Lobster Shack',
+    author: 'Aileen Slater',
+    imageSrc: '/assets/lobster_shack.jpg'
+  },
+  {
+    name: 'Praying Mantises',
+    author: 'Denny Mckenzie',
+    imageSrc: '/assets/praying_mantises.jpg'
+  },
+  {
+    name: 'Quality design',
+    author: 'Jude Combs',
+    imageSrc: '/assets/quality_design.jpg'
+  },
+  {
+    name: 'Reflect',
+    author: 'Jeffrey R. Reid',
+    imageSrc: '/assets/reflect.jpg'
+  },
+  {
+    name: 'Unleash The Dragon',
+    author: 'Robbie E. Pearson',
+    imageSrc: '/assets/unleash_the_dragon.jpg'
+  },
+  {
+    name: 'Washoku',
+    author: '',
+    imageSrc: '/assets/washoku.jpg'
   }
 ]
 
