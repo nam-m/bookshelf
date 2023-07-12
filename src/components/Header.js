e0c3e29daa06d22fd886df747c9430abf8664bd2
Nam Anh Mai <maithenamanh@gmail.com>
2023-07-11 23:10:50 -0700

    Add SearchBar to header and Nav for NavItems

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,50 @@ const Header = () => {
   return (
     <header>
       <Wrapper>
-        <HeaderLink href='/Home'>Home</HeaderLink>
-        <HeaderLink href='/Collections'>Collections</HeaderLink>
-        <HeaderLink href='/Notes'>Notes</HeaderLink>
+        <Nav>
+          <NavItem href='/Home'>Home</NavItem>
+          <NavItem href='/Collections'>Collections</NavItem>
+          <NavItem href='/Notes'>Notes</NavItem>
+          <SearchBar type='text' placeholder='Search...'/>
+        </Nav>
+        
       </Wrapper>
     </header>
   );
 };
 
-const Wrapper = styled.nav`
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
   padding: 48px;
+  padding-bottom: 32px;
   display: flex;
-  justify-content: center;
-  gap: 48px;
 `;
 
-const HeaderLink = styled.a`
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  /* gap: 48px; */
+`;
+
+const NavItem = styled.a`
   font-size: 1.5rem;
   text-decoration: none;
   color: black;
+`;
+
+const SearchBar = styled.input`
+  display: block;
+  width: auto-fit;
+  line-height: 1.5rem;
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  background-color: hsl(185deg, 10%, 95%);
+
+  &:active {
+    border: 1px solid hsl(220deg, 3%, 50%);
+  }
 `;
 
 export default Header;
