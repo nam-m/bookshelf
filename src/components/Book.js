b1d3d00ea6c900c56663d242fd0ad355f792c18d
Nam Anh Mai <maithenamanh@gmail.com>
2023-07-21 15:27:39 -0700

    Add popover button to preview book

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,12 +9,31 @@ const Book = ({name, author, imageSrc}) => {
       </ImageWrapper>
       <Name>{name}</Name>
       <Author>{author}</Author>
+      <PopoverInput type='submit' value='Quick look'/>
     </Link>
   );
 };
 
+
 const Link = styled.a`
   text-decoration: none;
+  position: relative;
+  cursor: pointer;
+`;
+
+const PopoverInput = styled.input`
+  /* display: none; */
+  
+  position: absolute;
+  bottom: 40px;
+  left: 0;
+  right: 0;
+  width: 95%;
+  margin: auto;
+  background-color: hsl(185deg, 5%, 95%);
+  border: 1px solid hsl(185deg, 5%, 50%);
+  border-radius: 8px;
+  cursor: pointer;
 `;
 
 const ImageWrapper = styled.div`
