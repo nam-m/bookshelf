fc8a50a0fd1476907d474ee196826d2fccc5935d
Nam Anh Mai <maithenamanh@gmail.com>
2023-08-22 01:40:44 -0700

    Need to match prop in PopoverWrapper with passed prop in App.js

--- src/components/common/PopoverWrapper.js
+++ src/components/common/PopoverWrapper.js
@@ -12,7 +12,7 @@ const PopoverWrapper = styled.div`
   background: hsl(0deg 0% 0% / 0.35);
 
   /* Make it visible when passed prop is true and vice versa*/
-  visibility: ${p => p.$show ? 'visible' : 'hidden'};
+  visibility: ${p => p.$showPreview ? 'visible' : 'hidden'};
 `;
 
 export default PopoverWrapper;
