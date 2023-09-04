263ad8bc12314b0cffd3b4b6f1f7edd31a511bb7
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-04 13:07:14 -0700

    Combine form and error states and move validate input to onChange listener directly

--- src/features/bookshelf/BookForm.js
+++ src/features/bookshelf/BookForm.js
@@ -1,169 +1,171 @@
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useState } from 'react';
 import styled from 'styled-components/macro';
-import CreateButton from "../../components/common/CreateButton";
+import CreateButton from '../../components/common/CreateButton';
 
 const BookForm = forwardRef(function BookForm({addBook, setAddBook}, ref) {
   // State and set state of form input fields
-  const [form, setForm] = useState({
-    book: "",
-    author: ""
-  });
-
   // Error state and set state when input is invalid
-    const [error, setError] = useState({
+    const [form, setForm] = useState({
       book: {
+        value: '',
         dirty: false,
         error: false,
-        message: ""
+        message: ''
       },
       author: {
+        value: '',
         dirty: false,
         error: false,
-        message: ""
+        message: ''
       }
     });
 
   // Update input fields onChange
   const onUpdateForm = (e) => {
+    // Perform validation here
+    let errorMessage = '';
+
+    if (e.target.name === 'book') {      
+      if (!e.target.value && form[e.target.name].dirty)
+        errorMessage = '*Author is required';
+    }
+    
+    else if (e.target.name === 'author') {
+      const re = new RegExp(/^[a-zA-Z.\-' ]+$/);
+      if (!e.target.value && form[e.target.name].dirty)
+        errorMessage = '*Author is required';
+      else if (!re.test(e.target.value))
+        errorMessage = '*Name must not contain digits or special characters';
+    }
+
     const updateFormState = {
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: {
+        ...form[e.target.name],
+        value: e.target.value,
+        dirty: true,
+        error: !!errorMessage,
+        message: errorMessage
+      }
     };
+      
     setForm(updateFormState);
+  }
+
+  const isFormValid = () => {
+    const validForm = Object.keys(form).reduce((result, key) => {
+      const {dirty, error, value} = form[key];
+      return result || (dirty && (error || value === ''));
+    }, false);
+
+    return validForm;
   }
 
   const handleSubmit = (e) => {
     // Prevent default submit
     e.preventDefault(); 
-
-    console.log(JSON.stringify(form, null, 2));
-
+    
     // Reset all fields in form
-    setForm({
-      book: "",
-      author: ""
-    });
+    if (isFormValid()) {
+      console.log('Valid form', form);
+      // Reset form
+      setForm({
+        book: {
+          value: '',
+          dirty: false,
+          error: false,
+          message: ''
+        },
+        author: {
+          value: '',
+          dirty: false,
+          error: false,
+          message: ''
+        }
+      });
+    }
+    else
+      alert('Invalid form', form);
+    
     // Set addBook state to false to trigger useClickOutside in App.js
     setAddBook(false);
   };
 
-  const validateBook = (e) => {
-    let errorMessage = "";
-    if (!e.target.value && error[e.target.name].dirty)
-      errorMessage = "*Author is required";
-    setError({
-      ...error,
-      book: {
-        ...error.book,
-        message: errorMessage
-      }
-    });
-  };
-
-  const validateAuthor = (e) => {
-    const re = new RegExp(/^[a-zA-Z.\-' ]+$/);
-    let errorMessage = "";
-    if (!e.target.value && error[e.target.name].dirty)
-      errorMessage = "*Author is required";
-    else if (!re.test(e.target.value))
-      errorMessage = "*Name must not contain digits or special characters";
-    setError({
-      ...error,
-      author: {
-        ...error.author,
-        message: errorMessage
-      }
-    });
-  }
-
-  // const validateForm = () => {
-  //   // Set flag to submit/not submit form
-  //   let valid = true;
-  //   Object.values(error).forEa
-  // };
+  
 
   return (addBook) ? (
     <Wrapper ref={ref}>
       <Form 
-        action="https://httpbin.org/post" 
-        method="post"
+        action='https://httpbin.org/post' 
+        method='post'
         // Open a new page on submit
-        target="_blank"
+        target='_blank'
         noValidate
         onSubmit={(e) => handleSubmit(e)}
       >
         <Row>
-          <BookLabel htmlFor="book">Book</BookLabel>
+          <BookLabel htmlFor='book'>Book</BookLabel>
           <BookInput 
-            type="text" 
-            id="book" 
-            name="book"
-            value={form.book}
-            onMouseDown={(e) => {
-              setError({
-                ...error, 
-                book: {
-                  ...error.book,
-                  dirty: true
-                }
-              });
-            }}
+            type='text' 
+            id='book' 
+            name='book'
+            value={form.book.value}
+            // onMouseDown={(e) => {
+            //   setForm({
+            //     ...form, 
+            //     book: {
+            //       ...form.book,
+            //       dirty: true
+            //     }
+            //   });
+            // }}
             onChange={e => {
               onUpdateForm(e);
-              validateBook(e);
             }}
             autoFocus
             >
           </BookInput>
-          {error.book.message && 
-            <ErrorMessage htmlFor="error">
-              {error.book.message}
+          {form.book.error && form.book.dirty &&
+            <ErrorMessage htmlFor='error'>
+              {form.book.message}
             </ErrorMessage>
           }
         </Row>
         <Row>
-          <AuthorLabel htmlFor="author">Author</AuthorLabel>
+          <AuthorLabel htmlFor='author'>Author</AuthorLabel>
           <AuthorInput 
-            type="text" 
-            id="author" 
-            name="author"
-            value={form.author}  
+            type='text' 
+            id='author' 
+            name='author'
+            value={form.author.value}  
             onChange={e => {
               onUpdateForm(e);
-              validateAuthor(e);
-              setError({
-                ...error, 
-                author: {
-                  ...error.author,
-                  dirty: true
-                }
-              });
             }}
           >
           </AuthorInput>
-          {error.author.message && 
-            <ErrorMessage htmlFor="error">
-              {error.author.message}
+          {form.author.error && form.author.dirty &&
+            <ErrorMessage htmlFor='error'>
+              {form.author.message}
             </ErrorMessage>
           }
         </Row>
         <Row>
           <NoteLabel 
-            htmlFor="notes"
+            htmlFor='notes'
           >
             Your Notes
           </NoteLabel>
           <NoteArea 
-            as="textarea"
-            id="notes" 
-            name="notes" 
-            rows="5" cols="33"
+            as='textarea'
+            id='notes' 
+            name='notes' 
+            rows='5' cols='33'
           >
           </NoteArea>
         </Row>
         <Row>
           <SubmitButton 
-            type="submit"
+            type='submit'
           >
             Add book
           </SubmitButton>
