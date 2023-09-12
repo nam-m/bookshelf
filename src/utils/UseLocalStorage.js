445abb7fa2e1ee91c5f8971f4866d77af2a6b30c
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-11 23:03:38 -0700

    Add useLocalStorage hook

---
+++ src/utils/UseLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+// Retrieve previous books, return defaultValue if not found any
+const getStorageData = (key, defaultValue) =>{
+  const savedItem = localStorage.getItem(key);
+return JSON.parse(savedItem) || defaultValue;
+}
+
+// Set initialValue to savedItem or defaultValue in localStorage
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    return getStorageData(key, initialValue);
+  });
+
+// Save new key-value pair to localStorage every time key or value changes
+useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+ 
+return [value, setValue];
+}
+
+export default useLocalStorage;
