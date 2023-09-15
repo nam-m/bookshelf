65f18a7535789c8b5ac73a34f0ba18c3e29c5fd5
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-15 13:26:54 -0700

    Put some info in footer just to know where it is

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -4,7 +4,11 @@ import styled from 'styled-components/macro'
 const Footer = () => {
     return (
       <footer>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <Info>
+            &copy;2023 Nam Mai | All Rights Reserved
+          </Info>
+        </Wrapper>
       </footer>
     );
 };
@@ -16,6 +20,13 @@ const Wrapper = styled.div`
   margin-right: auto;
   padding: 0 80px;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid hsl(120deg 5% 5%);
+`;
+
+const Info = styled.p`
+
 `;
 
 export default Footer;
