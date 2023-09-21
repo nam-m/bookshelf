ef68ff955f71ee110a06abad817f59575916b676
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-21 16:38:46 -0700

    Add editing flag

--- src/data/ShelfData.js
+++ src/data/ShelfData.js
@@ -2,16 +2,19 @@ const SHELVES = [
   {
     id: 'shelf-0',
     name: 'Shelf 0',
+    isEditing: false,
     books: [],
   },
   {
     id: 'shelf-1',
     name: 'Shelf 1',
+    isEditing: false,
     books: [],
   },
   {
     id: 'shelf-2',
     name: 'Shelf 2',
+    isEditing: false,
     books: [],
   },
 ]
