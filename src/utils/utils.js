3fc7439b7f0f83f9cca37388a5bbe4ef75702f9e
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-22 15:51:48 -0700

    more function in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,12 @@
-function updateObjectInArray(array, id, key, newValue) {
+export function updateObjectInArray(array, id, key, newValue) {
   return array.map(item => {
-    if (item.id === id) {
+    if (item.id === id && item.value !== newValue) {
       return { ...item, [key]: newValue };
     }
     return item;
   });
 }
 
-export default updateObjectInArray;
+export function getObjectValueinArray(array, id, key) {
+  return array.find(item => item.id === id )[key];
+}
