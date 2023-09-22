0e1df8c7b943fc7c9ceaac2443f8d79691544fa6
Nam Anh Mai <maithenamanh@gmail.com>
2023-09-21 23:33:06 -0700

    Add react-feather and global icon wrapper

---
+++ src/components/common/Icon.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import {
+  Check,
+  X,
+  Edit2
+} from 'react-feather';
+
+const icons = {
+  check: Check,
+  cancel: X,
+  edit: Edit2
+};
+
+const Icon = ({ id, color, size, strokeWidth, ...delegated }) => {
+  const Component = icons[id];
+
+  if (!Component) {
+    throw new Error(`No icon found for ID: ${id}`);
+  }
+
+  return (
+    <Wrapper strokeWidth={strokeWidth} {...delegated}>
+      <Component color={color} size={size} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > svg {
+    display: block;
+    stroke-width: ${(p) => p.strokeWidth}px;
+  }
+`;
+
+export default Icon;
